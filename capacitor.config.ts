c4b2a919ae9563a29117a404dedc192833274762
chidibede <chidibede@gmail.com>
2021-08-21 23:20:15 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'business-management-platform',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
